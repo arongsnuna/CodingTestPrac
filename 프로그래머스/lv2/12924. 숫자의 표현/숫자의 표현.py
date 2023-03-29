391075a23bbd4e5350ea5202b1dc238b04fd43f9
Unhi Park <unhi000@naver.com>
2023-03-29 23:59:49 +0900

    [level 2] Title: 숫자의 표현, Time: 5.71 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,23 @@
+def solution(n):
+    answer = 0
+    for i in range(1,n+1):
+        sum=0
+        for j in range(i,n+1):
+            sum += j
+            if (sum==n):
+                answer +=1
+                break
+            elif (sum>n):
+                break
+    return answer
+'''
+def solution(n):
+    answer = 0
+    
+    for i in range(n,0,-1):
+        count = n
+        for j in range(i,0,-1):
+            count-=j
+            if(count == 0): answer+=1
+    return answer
+'''
