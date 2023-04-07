abf3d97e7a02a23f2bfea55718126735affce394
Unhi Park <unhi000@naver.com>
2023-04-08 01:49:25 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,14 +1,8 @@
+
 def solution(num):
-    count = 0
-    while (count <500 and num!=1):
-        if(num%2==0):
-            num /= 2
-            count +=1
-        else:
-            num = num*3 +1
-            count +=1
-    if(count==500):
-        return -1
-    else:
-        return count
-            
+    if(num==1): return 0
+    for i in range(500):
+        num = num/2 if (num%2==0) else num*3+1
+        if(num==1):
+            return i+1
+    return -1
