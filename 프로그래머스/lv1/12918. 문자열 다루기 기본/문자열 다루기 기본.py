0ef2f2fb5d39cc9a4ee46a69febd861fb655c7a2
Unhi Park <unhi000@naver.com>
2023-04-11 03:00:47 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,6 +1,10 @@
 def solution(s):
+    return s.isdigit() and len(s) in [4,6]
+'''
+def solution(s):
     answer=False
     if (len(s)==4 or len(s)==6):
         if(s.isdigit()):
             answer = True
     return answer
+'''
