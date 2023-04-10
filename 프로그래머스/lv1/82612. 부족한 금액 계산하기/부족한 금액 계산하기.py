06c5998bd981994a3f8089be91195cef5ec496e3
Unhi Park <unhi000@naver.com>
2023-04-11 03:05:43 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -1,7 +1,9 @@
 def solution(price, money, count):
+    return max(0, price*(count+1)*(count)//2-money)
+    '''
     hap = 0
     for i in range(1,count+1):
         hap += price*i
     answer = 0 if(money >hap) else hap-money
 
-    return answer
+    return answer'''
